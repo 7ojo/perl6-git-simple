c199be6373daf4b1b11b35cb729d00665a325e6d
Jarkko Haapalainen <jarkko@viidakko.fi>
2017-03-07 22:27:27 +0200

    added support for git init and local only branch

--- lib/Git/Simple.pm
+++ lib/Git/Simple.pm
@@ -5,9 +5,19 @@ class Git::Simple {
 
     has Str $.cwd = '.';
 
-    method branch-info {
+    method branch-info returns Hash {
         my $proc = run <git -C>, $.cwd, <status --porcelain -b>, :out, :err;
-        Git::Simple::Parse.new.status(out => $proc.out.slurp-rest);
+        return Hash.new if $proc.err.slurp-rest.lines;
+        my %info = Git::Simple::Parse.new.status(out => $proc.out.slurp-rest);
+        if (%info.elems == 0) { # detached branch
+            $proc = run <git -C>, $.cwd, <describe --tags --always>, :out, :err;
+            if ($proc.err.slurp-rest.lines) {
+                %info<local> = 'Big Bang'; # initial repository
+            } else {
+                %info<local> = $proc.out.slurp-rest.lines[0]; # right?
+            }
+        }
+        %info;
     }
 
 }
---
+++ t/20-detached.t
@@ -0,0 +1,22 @@
+use v6;
+use lib 'lib';
+use Test;
+use Git::Simple;
+
+plan 2;
+
+my $dir = '/tmp/.test-git-simple-20-git-detached';
+mkdir $dir;
+my $proc = run <git -C>, $dir, <init>, :out;
+
+my %branch-info = Git::Simple.new(cwd => $dir).branch-info;
+is %branch-info<local>, 'Big Bang', 'git init';
+
+run <touch>, "$dir/foo.txt";
+run <git -C>, $dir, <add>, "$dir/foo.txt", :out;
+run <git -C>, $dir, <commit -m>, "added foo.txt", :out;
+
+%branch-info = Git::Simple.new(cwd => $dir).branch-info;
+is %branch-info<local>, 'master', 'git add + commit';
+
+run <rm -rf>, $dir;
